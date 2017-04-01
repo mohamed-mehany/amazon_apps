24f9c6289ffdc250cce619fb912ad9ad4d306139
Abkreno <mohamedelhamamsy789@gmail.com>
2017-04-01 23:45:38 +0200

    Modifies Request Parse:Initializes ClientRequest/ Calls ParsingFinished

--- src/RequestParser.java
+++ src/RequestParser.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class RequestParser implements Runnable {
 
 	protected ParseListener _parseListener;
@@ -11,7 +14,14 @@ public class RequestParser implements Runnable {
 	public void run() {
 		try {
 			// CALL DISPATCHER
+			Map<String, Object> data = new HashMap<String, Object>();
+			data.put("email", "mohamed@m.com");
+			data.put("password", "johnpass");
+			data.put("firstName", "mohamed");
+			data.put("lastName", "mostafa");
 			
+			ClientRequest clientRequest = new ClientRequest("addUserSimple", "121", data);
+			_parseListener.parsingFinished(_clientHandle, clientRequest);
 		} catch (Exception exp) {
 			_parseListener.parsingFailed(_clientHandle,
 					"Exception while parsing JSON object " + exp.toString());
