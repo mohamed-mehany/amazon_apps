fb1198cb28b37e35fb24f54e12b5a674e69919c4
Merna Waseif <mernawaseif@Mernas-MacBook-Pro.local>
2017-04-15 00:16:38 +0200

    login updated

--- src/commands/user/LoginCmd.java
+++ src/commands/user/LoginCmd.java
@@ -11,43 +11,43 @@ import commands.Command;
 import services.Cache;
 
 public class LoginCmd extends Command implements Runnable {
-
-	public StringBuffer execute(Connection connection, Map<String, Object> mapUserData)
-			throws Exception {
-
-		CallableStatement sqlProc;
-		StringBuffer strbufResult = null, strbufResponseJSON;
-		String strSessionID, strEmail, strPassword, strFirstName, strClientIP;
-		int nSQLResult;
-
-		strEmail = ((String) mapUserData.get("email"));
-		strPassword = ((String) mapUserData.get("password"));
-
-		if (strEmail == null || strEmail.trim().length() == 0 || strPassword == null
-				|| strPassword.trim().length() == 0)
-			return null;
-
-		if (!EmailVerifier.verify(strEmail))
-			return null;
-
-		sqlProc = connection.prepareCall("{?=call user_login(?,?)}");
-		sqlProc.registerOutParameter(1, Types.INTEGER);
-		sqlProc.setString(2, strEmail);
-		sqlProc.setString(3, strPassword);
-		sqlProc.execute();
-		nSQLResult = sqlProc.getInt(1);
-		sqlProc.close();
-		if (nSQLResult >= 0) {
-			System.out.println("user logged in successfully");
-			System.err.println(" user logged in successfully" ); 
-		
-		} else{
-			System.out.println(" user failed to log in " );
-			System.err.println(" user failed to log in " );
-		}
-			strbufResult = makeJSONResponseEnvelope(0, null, null);
-			//strbufResult= String.valueOf(nSQLResult);
-
-		return strbufResult;
-	}
+    
+    public StringBuffer execute(Connection connection, Map<String, Object> mapUserData)
+    throws Exception {
+        
+        CallableStatement sqlProc;
+        StringBuffer strbufResult = null, strbufResponseJSON;
+        String strSessionID, strEmail, strPassword, strFirstName, strClientIP;
+        int nSQLResult;
+        
+        strEmail = ((String) mapUserData.get("email"));
+        strPassword = ((String) mapUserData.get("password"));
+        
+        if (strEmail == null || strEmail.trim().length() == 0 || strPassword == null
+            || strPassword.trim().length() == 0)
+            return null;
+        
+        if (!EmailVerifier.verify(strEmail))
+            return null;
+        
+        sqlProc = connection.prepareCall("{call user_login(?,?,?)}");
+        sqlProc.setString(1, strEmail);
+        sqlProc.setString(2, strPassword);
+        sqlProc.registerOutParameter(3, Types.INTEGER);
+        sqlProc.execute();
+        nSQLResult = sqlProc.getInt(3);
+        sqlProc.close();
+        if (nSQLResult >= 0) {
+            System.out.println("user logged in successfully");
+            System.err.println(" user logged in successfully" );
+            
+        } else{
+            System.out.println(" user failed to log in " );
+            System.err.println(" user failed to log in " );
+        }
+        strbufResult = makeJSONResponseEnvelope(0, null, null);
+        //strbufResult= String.valueOf(nSQLResult);
+        
+        return strbufResult;
+    }
 }
