93dc6a2f89ad51565d11269fe154af372d6c65e1
omar <omarelhariry137@gmail.com>
2017-04-14 21:42:22 +0200

    Adding command viewUserOrders: user can view his orders

--- src/controller/RequestParser.java
+++ src/controller/RequestParser.java
@@ -1,7 +1,10 @@
+
+
 package controller;
 
 import java.nio.charset.Charset;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import org.boon.json.implementation.JsonStringDecoder;
@@ -14,38 +17,43 @@ import io.netty.util.CharsetUtil;
 
 public class RequestParser implements Runnable {
 
-	protected ParseListener _parseListener;
-	protected ClientHandle _clientHandle;
+  protected ParseListener _parseListener;
+  protected ClientHandle _clientHandle;
 
-	public RequestParser(ParseListener parseListener, ClientHandle clientHandle) {
-		_parseListener = parseListener;
-		_clientHandle = clientHandle;
-	}
+  public RequestParser(ParseListener parseListener, ClientHandle clientHandle) {
+    _parseListener = parseListener;
+    _clientHandle = clientHandle;
+  }
 
-	public void run() {
-		try {
-			// CALL DISPATCHER
-			HttpContent cc = (HttpContent) (_clientHandle._httpRequest);
-			ByteBuf content = cc.content();
-			JsonObject body = JsonObject.readFrom(content.toString(CharsetUtil.UTF_8));
-			JsonObject jsonData = body.get("data").asObject();
-			String command = body.get("command").asString();
-			String sessionID = body.get("sessionID").asString();
-			Map<String, Object> data = new HashMap<String, Object>();
-			data.put("email", jsonData.get("email").asString());
-			data.put("password", jsonData.get("password").asString());
-			data.put("firstName", jsonData.get("firstName").asString());
-			data.put("lastName", jsonData.get("lastName").asString());
-			data.put("address", jsonData.get("address").asString());
-			data.put("date", jsonData.get("date").asString());
-			data.put("token", jsonData.get("token").asString());
-			data.put("gender", jsonData.get("gender").asInt());
-			ClientRequest clientRequest = new ClientRequest(command, sessionID, data);
-			_parseListener.parsingFinished(_clientHandle, clientRequest);
-		} catch (Exception exp) {
-			_parseListener.parsingFailed(_clientHandle,
-					"Exception while parsing JSON object " + exp.toString());
-		}
-	}
+  public void run() {
+    try {
+      // CALL DISPATCHER
+
+      HttpContent cc = (HttpContent) (_clientHandle._httpRequest);
+      ByteBuf content = cc.content();
+      JsonObject body = JsonObject.readFrom(content.toString(CharsetUtil.UTF_8));
+      JsonObject jsonData = body.get("data").asObject();
+      String command = body.get("command").asString();
+      String sessionID = body.get("sessionID").asString();
+      Map<String, Object> data = new HashMap<String, Object>();
+      Iterator<JsonObject.Member> temp = jsonData.iterator();
+      while(temp.hasNext()){
+        JsonObject.Member x = temp.next();
+        if (x.getValue().isNumber()){
+          data.put(x.getName(), x.getValue().asInt());
+
+        } else if (x.getValue().isString()) {
+          data.put(x.getName(), x.getValue().asString());
+        }
+      }
+//      System.out.println("*********** "+data.toString());
+
+      ClientRequest clientRequest = new ClientRequest(command, sessionID, data);
+      _parseListener.parsingFinished(_clientHandle, clientRequest);
+    } catch (Exception exp) {
+      _parseListener.parsingFailed(_clientHandle,
+          "Exception while parsing JSON object " + exp.toString());
+    }
+  }
 
 }
